700a15c96a0c07a5c89f81321f7624fdace93190
greenyarnball <77083012+greenyarnball@users.noreply.github.com>
2023-05-01 08:57:10 +0900

    ドリンクメニュー内容追加

--- js/folding.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('.toggle_title').on('click',function() {
-		$(this).toggleClass('selected');
-		$(this).next().slideToggle();
-		$('.toggle_title').not($(this)).next().slideUp();
-		$('.toggle_title').not($(this)).removeClass('selected');
-	});
-});
